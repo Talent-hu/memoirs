8a2e8e5e49e2c40a680b8e59002172c28d93d66b
talent <1721792064@qq.com>
2022-07-30 21:47:39 +0800

    重新配置跨域

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,7 +9,7 @@ func Cors() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		method := ctx.Request.Method                                                                                                           //请求方法
 		ctx.Header("Access-Control-Allow-Origin", ctx.GetHeader("Origin"))                                                                     // 指明哪些请求源被允许访问资源，值可以为 "*"（允许访问所有域），"null"，或者单个源地址。
-		ctx.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")                              // 对于预请求来说，指明了哪些头信息可以用于实际的请求中。
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, AuthToken")                          // 对于预请求来说，指明了哪些头信息可以用于实际的请求中。
 		ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")                                                                       // 对于预请求来说，哪些请求方式可以用于实际的请求。
 		ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type") // 对于预请求来说，指明哪些头信息可以安全的暴露给 CORS API 规范的 API
 		ctx.Header("Access-Control-Allow-Credentials", "true")                                                                                 // 指明当请求中省略 creadentials 标识时响应是否暴露。对于预请求来说，它表明实际的请求中可以包含用户凭证。
