d39943c3b26a0e42e2cda64c95fb19825c577388
hutianhua <911680803@qq.com>
2022-07-31 11:58:58 +0800

    添加Jenkinsfile 脚本文件 7

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -20,12 +20,13 @@ pipeline {
             steps {
                 sh ''' 
                     kill -9 $(netstat -antp | grep :8888 | awk '{print $7}' | awk -F'/' '{ print $1 }') 
+                  
                    '''
             }
         }
         stage('运行项目') {
             steps {
-                sh ''' nohup /home/study/memoirs > memoirs.log & '''
+                sh ''' nohup /home/study/memoirs & '''
             }
         }
     }
