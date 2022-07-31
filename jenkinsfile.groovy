b47f0334363daf33bafabd7f7cfb804e32897646
hutianhua <911680803@qq.com>
2022-07-31 11:33:57 +0800

    添加Jenkinsfile 脚本文件 1

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,30 +2,30 @@ pipeline {
     agent any
     stages {
 
-         stage('拉取文件') {
+        stage('拉取文件') {
             steps {
-             git credentialsId: '18387188346', url: 'https://gitee.com/hutiancai/memo.git'
+                git credentialsId: '18387188346', url: 'https://gitee.com/hutiancai/memo.git'
             }
-         }
-         stage('编译代码') {
-             steps {
-                sh'''
+        }
+        stage('编译代码') {
+            steps {
+                sh '''
                    echo ${WORKSPACE}
                    cd  ${WORKSPACE}
-                   go build 
+                   go build -o /home/memoirs 
                   '''
-             }
-         }
-         stage('停止项目') {
-             steps {
-             echo '停止项目'
-             }
-         }
-         stage('运行项目') {
-             steps {
-                echo '复制项目'
-             }
-         }
+            }
+        }
+        stage('停止项目') {
+            steps {
+                sh ''' kill -9 $(netstat -antp | grep :8888 | awk '{print $7}' | awk -F'/' '{ print $1 }') '''
+            }
+        }
+        stage('运行项目') {
+            steps {
+                sh ''' nohup ./memoirs & '''
+            }
+        }
     }
 
 }
